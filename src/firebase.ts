3484a69ba83d168b727cefe61964c8f9a90e8c1c
Aviva <avivajoy.gars@gmail.com>
2023-01-30 13:35:27 -0700

    testing github

---
+++ src/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCChvh1y3UFXxgjczYFj2yo4cX_wYGI5kQ",
+  authDomain: "under-the-hood-a40cc.firebaseapp.com",
+  projectId: "under-the-hood-a40cc",
+  storageBucket: "under-the-hood-a40cc.appspot.com",
+  messagingSenderId: "529643374783",
+  appId: "1:529643374783:web:376acbb7d8542d36065e0a",
+  measurementId: "G-V7ERLS36F3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
