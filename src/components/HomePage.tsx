8cf8444eb7cf2833519abcac69f2fe29bf470226
Aviva <avivajoy.gars@gmail.com>
2023-01-30 17:08:56 -0700

    worked on homepage

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,7 +1,17 @@
-// import { Button } from "react-bootstrap"
+import { Button, Container } from "react-bootstrap"
 export function HomePage () {
     return (
-        <div>Home</div>
+        <div> 
+            <h1 className= "text-center">Get word from a specialist</h1>
+            <h2 className="text-center"> What type of help are you looking for?</h2>
+            <div className="container">
+                <div className="text-center">
+                    <Button type="button" className="btn btn-primary btn-lg">Pricing</Button>
+                    <Button className="btn btn-primary btn-lg">Diagnostics</Button>
+                    <Button className="btn btn-primary btn-lg">Other</Button>
+                </div>
+            </div> 
+            </div>
     )
 }
 
